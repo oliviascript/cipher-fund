e495e01e40813bb73ec285bd02945f69fc8a61cf
oliviascript <AliottaCleath129@hotmail.com>
2025-10-23 19:56:06 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,32 @@
-import { DeployFunction } from "hardhat-deploy/types";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as dotenv from "dotenv";
+import type { DeployFunction } from "hardhat-deploy/types";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  log(`Deployer: ${deployer}`);
+  log(`INFURA key configured: ${process.env.INFURA_API_KEY ? "yes" : "no"}`);
+
+  const tokenDeployment = await deploy("ERC7984ETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const fundraisingDeployment = await deploy("ConfidentialFundraising", {
+    from: deployer,
+    args: [tokenDeployment.address],
+    log: true,
+  });
+
+  log(`ConfidentialFundraising deployed at ${fundraisingDeployment.address}`);
+  log(`ERC7984ETH deployed at ${tokenDeployment.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidential_fundraising";
+func.tags = ["Fundraising"];
